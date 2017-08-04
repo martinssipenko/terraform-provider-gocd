9952e897b4336640e3a8ce5997a59e75a629f5f4
Drew J. Sonne <drew.sonne@gmail.com>
2017-08-04 22:36:53 +0100

    Fixed linting issues

--- gocd/data_source_job_definition_test.go
+++ gocd/data_source_job_definition_test.go
@@ -11,8 +11,8 @@ func TestDataSourceJobDefinition(t *testing.T) {
 	for i := 0; i <= 1; i++ {
 		testSteps = append(
 			testSteps,
-			testStepComparisonCheck(TestStepJsonComparison{
-				Id:           "data.gocd_job_definition.test",
+			testStepComparisonCheck(TestStepJSONComparison{
+				ID:           "data.gocd_job_definition.test",
 				Config:       testFile(fmt.Sprintf("data_source_job_template.%d.rsc.tf", i)),
 				ExpectedJSON: testFile(fmt.Sprintf("data_source_job_template.%d.rsp.json", i)),
 			}),
