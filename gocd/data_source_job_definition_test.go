b2ca2e56d910e7f2ab720301adca7d76eeddef18
Drew J. Sonne <drew.sonne@gmail.com>
2017-08-05 12:53:37 +0100

    Autocommit for 'gofmt -w'.

--- gocd/data_source_job_definition_test.go
+++ gocd/data_source_job_definition_test.go
@@ -7,22 +7,31 @@ import (
 )
 
 func TestDataSourceJobDefinition(t *testing.T) {
-	testSteps := []resource.TestStep{}
+
 	for i := 0; i <= 1; i++ {
-		testSteps = append(
-			testSteps,
-			testStepComparisonCheck(&TestStepJSONComparison{
-				Index:        i,
-				ID:           "data.gocd_job_definition.test",
-				Config:       testFile(fmt.Sprintf("data_source_job_definition.%d.rsc.tf", i)),
-				ExpectedJSON: testFile(fmt.Sprintf("data_source_job_definition.%d.rsp.json", i)),
-			}),
+		t.Run(
+			fmt.Sprintf("gocd_job_definition.%d", i),
+			DataSourceJobDefinition(t, i,
+				fmt.Sprintf("data_source_job_definition.%d.rsc.tf", i),
+				fmt.Sprintf("data_source_job_definition.%d.rsp.json", i),
+			),
 		)
 	}
+}
 
-	resource.Test(t, resource.TestCase{
-		PreCheck:  func() { testAccPreCheck(t) },
-		Providers: testGocdProviders,
-		Steps:     testSteps,
-	})
+func DataSourceJobDefinition(t *testing.T, index int, configPath string, expectedPath string) func(t *testing.T) {
+	return func(t *testing.T) {
+		config := testFile(configPath)
+		expected := testFile(expectedPath)
+		resource.Test(t, resource.TestCase{
+			PreCheck:  func() { testAccPreCheck(t) },
+			Providers: testGocdProviders,
+			Steps: []resource.TestStep{testStepComparisonCheck(&TestStepJSONComparison{
+				Index:        index,
+				ID:           "data.gocd_job_definition.test",
+				Config:       config,
+				ExpectedJSON: expected,
+			})},
+		})
+	}
 }
