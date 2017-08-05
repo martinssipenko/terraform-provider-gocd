d90acac38cd0c8ef424cbc12449e53a7bb651178
Drew J. Sonne <drew.sonne@gmail.com>
2017-08-05 12:41:35 +0100

    Added subtests for json matching

--- gocd/data_source_task_definition_test.go
+++ gocd/data_source_task_definition_test.go
@@ -8,21 +8,30 @@ import (
 
 func TestDataSourceTaskDefinition(t *testing.T) {
 
-	testSteps := []resource.TestStep{}
-	for i := 0; i <= 5; i++ {
-		testSteps = append(
-			testSteps,
-			testStepComparisonCheck(&TestStepJSONComparison{
-				ID:           "data.gocd_task_definition.test",
-				Config:       testFile(fmt.Sprintf("data_source_task_definition.%d.rsc.tf", i)),
-				ExpectedJSON: testFile(fmt.Sprintf("data_source_task_definition.%d.rsp.json", i)),
-			}),
+	for i := 0; i <= 5; i ++ {
+		t.Run(
+			fmt.Sprintf("gocd_task_definition.%d", i),
+			DataSourceTaskDefinition(t,
+				fmt.Sprintf("data_source_task_definition.%d.rsc.tf", i),
+				fmt.Sprintf("data_source_task_definition.%d.rsp.json", i),
+			),
 		)
 	}
+}
 
-	resource.Test(t, resource.TestCase{
-		PreCheck:  func() { testAccPreCheck(t) },
-		Providers: testGocdProviders,
-		Steps:     testSteps,
-	})
+func DataSourceTaskDefinition(t *testing.T, configPath string, expectedPath string) func(t *testing.T) {
+	return func(t *testing.T) {
+		config := testFile(configPath)
+		expected := testFile(expectedPath)
+		resource.Test(t, resource.TestCase{
+			PreCheck:  func() { testAccPreCheck(t) },
+			Providers: testGocdProviders,
+			Steps: []resource.TestStep{testStepComparisonCheck(&TestStepJSONComparison{
+				ID:           "data.gocd_task_definition.test",
+				Config:       config,
+				ExpectedJSON: expected,
+			})},
+		})
+	}
+
 }
