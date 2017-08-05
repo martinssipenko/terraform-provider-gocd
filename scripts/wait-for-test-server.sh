4e9c22555b4c0d3f150e238b9f6debf9d093248e
Drew J. Sonne <drew.sonne@gmail.com>
2017-08-05 11:47:21 +0100

    Added integration tests

---
+++ scripts/wait-for-test-server.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+function get_status {
+    curl -H 'Accept: application/vnd.go.cd.v3+json' \
+        --write-out %{http_code} \
+        --silent \
+        --output /dev/null \
+        http://localhost:8153/go/api/admin/templates
+}
+
+counter=0
+wait_length=5
+
+while [ $counter -lt 30 ]; do
+
+    code=$(get_status)
+    if [ "200" == "$code" ]; then
+        echo "Got status ${code}. Exiting..."
+        exit 0
+    fi
+
+    echo "Got status ${code}. Waiting for '${wait_length}' seconds..."
+
+    sleep "${wait_length}"
+
+done
+
+exit 1
