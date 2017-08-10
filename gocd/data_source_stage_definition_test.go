1f0c4aa5e0422929f791881f4ac6cb7680c974ce
Drew J. Sonne <drew.sonne@gmail.com>
2017-08-10 20:56:34 +0100

    Expanded test coverage

--- gocd/data_source_stage_definition_test.go
+++ gocd/data_source_stage_definition_test.go
@@ -4,7 +4,45 @@ import (
 	"fmt"
 	"github.com/hashicorp/terraform/helper/resource"
 	"testing"
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/stretchr/testify/assert"
 )
+
+func TestDataSourceGocdStageTemplateRead(t *testing.T) {
+	t.Run("success", testDataSourceGocdStageTemplateReadSuccess)
+	t.Run("fail", testDataSourceGocdStageTemplateReadFail)
+}
+func testDataSourceGocdStageTemplateReadFail(t *testing.T) {
+	s := dataSourceGocdStageTemplate().Schema
+	d := map[string]interface{}{
+		"success_approval": true,
+		"jobs":             []string{"one", "two"},
+	}
+	rd := schema.TestResourceDataRaw(t, s, d)
+	err := dataSourceGocdStageTemplateRead(rd, nil)
+	assert.NotNil(t, err)
+}
+func testDataSourceGocdStageTemplateReadSuccess(t *testing.T) {
+	s := dataSourceGocdStageTemplate().Schema
+	d := map[string]interface{}{
+		"name":             "one",
+		"success_approval": true,
+		"jobs":             []string{"one", "two"},
+	}
+	rd := schema.TestResourceDataRaw(t, s, d)
+	err := dataSourceGocdStageTemplateRead(rd, nil)
+	assert.Nil(t, err)
+	assert.Equal(t, `{
+  "name": "one",
+  "fetch_materials": false,
+  "clean_working_directory": false,
+  "never_cleanup_artifacts": false,
+  "approval": {
+    "type": "success"
+  }
+}`, rd.Get("json"))
+
+}
 
 func TestDataSourceStageTemplate(t *testing.T) {
 	for i := 0; i <= 0; i++ {
