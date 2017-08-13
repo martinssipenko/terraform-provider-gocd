30ae1f08719e3cefd8dfc23dfa93dd18968fd590
Drew J. Sonne <drew.sonne@gmail.com>
2017-08-13 11:18:43 +0100

    Made sure non-integration tests were UnitTests

--- gocd/data_source_task_definition_test.go
+++ gocd/data_source_task_definition_test.go
@@ -22,7 +22,7 @@ func DataSourceTaskDefinition(t *testing.T, index int, configPath string, expect
 	return func(t *testing.T) {
 		config := testFile(configPath)
 		expected := testFile(expectedPath)
-		r.Test(t, r.TestCase{
+		r.UnitTest(t, r.TestCase{
 			PreCheck:  func() { testAccPreCheck(t) },
 			Providers: testGocdProviders,
 			Steps: testStepComparisonCheck(&TestStepJSONComparison{
