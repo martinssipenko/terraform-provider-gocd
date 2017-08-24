1ada0920dbdbd900fdc82916bc9108dd0d9f3cab
Drew J. Sonne <drew.sonne@gmail.com>
2017-08-24 18:12:44 -0500

    Added test to ensure name is set

---
+++ gocd/test/resource_pipeline.0.rsc.tf
@@ -0,0 +1,38 @@
+resource "gocd_pipeline" "test-pipeline" {
+  name = "pipeline0-terraform"
+  template = "${gocd_pipeline_template.test-pipeline.id}"
+}
+
+
+resource "gocd_pipeline_template" "test-pipeline" {
+  name = "template0-terraform"
+  stages = [
+    <<STAGE
+{
+  "name": "test-stage",
+  "fetch_materials": false,
+  "clean_working_directory": false,
+  "never_cleanup_artifacts": false,
+  "approval": {
+    "type": "success"
+  },
+  "jobs": [
+    {
+      "name": "job1",
+      "tasks": [
+        {
+          "type": "exec",
+          "attributes": {
+            "run_if": [
+              "passed"
+            ],
+            "command": "terraform"
+          }
+        }
+      ]
+    }
+  ]
+}
+STAGE
+  ]
+}
