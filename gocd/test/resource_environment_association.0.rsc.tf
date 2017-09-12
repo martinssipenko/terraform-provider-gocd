ed22f66ecdc066f42472fdc178f672961faeb299
Drew J. Sonne <drew.sonne@gmail.com>
2017-09-12 20:04:18 +0100

    Added association test skeleton

---
+++ gocd/test/resource_environment_association.0.rsc.tf
@@ -0,0 +1,25 @@
+resource "gocd_environment" "test-environment" {
+  name = "test-environment"
+}
+
+resource "gocd_pipeline" "test-pipeline" {
+  name = "test-pipeline"
+  group = "test-group"
+  materials = [
+    {
+      type = "git"
+      attributes {
+        name = "gocd-src"
+        url = "git@github.com:gocd/gocd"
+        branch = "master"
+        auto_update = "true"
+      }
+    }
+  ]
+}
+
+resource "gocd_environment_association" "test-environment-association" {
+  name = "test-pipeline-environment"
+  environment = "${gocd_environment.test-environment.name}"
+  pipeline = "${gocd_pipeline.test-pipeline.name}"
+}
